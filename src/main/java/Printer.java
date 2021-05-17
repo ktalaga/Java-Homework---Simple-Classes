30696304160254ebf9b20c19ddc94da5b055609a
konrad <ktalaga90@gmail.com>
2021-05-17 20:55:21 +0100

    canReduceTonerVolume test passed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,17 +1,28 @@
 public class Printer {
 
-    public int sheetsLeft;
+    private int sheetsLeft;
+    private int tonerVolume;
 
 
-    public Printer(int sheetsLeft){
+    public Printer(int sheetsLeft, int tonerVolume){
         this.sheetsLeft = sheetsLeft;
+        this.tonerVolume = tonerVolume;
 
+    }
+
+    public int hasSheetsLeft(){
+        return this.sheetsLeft;
+    }
+
+    public int hasTonerVolume(){
+        return this.tonerVolume;
     }
 
     public void print(int numberOfPages, int numberOfCopies){
         int sheetsUsed = numberOfCopies * numberOfPages;
         if(numberOfCopies * numberOfPages <= this.sheetsLeft){
             this.sheetsLeft = this.sheetsLeft - sheetsUsed;
+            this.tonerVolume = this.tonerVolume - sheetsUsed;
         }
 
 
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,18 +9,24 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer(1000);
+        printer = new Printer(1000, 10000);
     }
 
     @Test
     public void hasSheetsLeft(){
-        assertEquals(1000, printer.sheetsLeft);
+        assertEquals(1000, printer.hasSheetsLeft());
     }
 
     @Test
     public void canPrint(){
-        printer.print(100, 2);
-        assertEquals(800, printer.sheetsLeft);
+        printer.print(200, 2);
+        assertEquals(600, printer.hasSheetsLeft());
+    }
+
+    @Test
+    public void canReduceTonerVolume(){
+        printer.print(50, 5);
+        assertEquals(9750, printer.hasTonerVolume());
     }
 
 }
